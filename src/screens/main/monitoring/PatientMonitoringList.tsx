8d33468207eb32cad94f1b3b0cc8201414c8f67c
Joker-40 <samuip8@gmail.com>
2024-07-28 03:47:47 +0530

    Empty screen

--- src/screens/main/monitoring/PatientMonitoringList.tsx
+++ src/screens/main/monitoring/PatientMonitoringList.tsx
@@ -1,5 +1,12 @@
 /* eslint-disable react/no-unstable-nested-components */
-import {FlatList, SafeAreaView, StyleSheet, View} from 'react-native';
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import React from 'react';
 import Header from '../../../components/header/Header';
 import {Colors} from '../../../constants/Colors';
@@ -9,22 +16,48 @@ import Card from '../../../components/cards/Card';
 import FilledButton from '../../../components/buttons/FilledButton';
 import Icons from '../../../assets/icon/Icon';
 import {navigate} from '../../../navigation/RootNavigation';
+import {StaticImage} from '../../../assets/images';
 
 const PatientMonitoringList = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header title="Monitoring" showBackIcon={false} />
       <View style={styles.contentContainer}>
-        <Card>
-          <FlatList
-            data={patientList}
-            showsVerticalScrollIndicator={false}
-            renderItem={({item, index: _index}) => (
-              <PatientListItem name={item.name} />
-            )}
-            ItemSeparatorComponent={() => <View style={styles.separator} />}
-          />
-        </Card>
+        {patientList?.length > 0 ? (
+          <Card>
+            <FlatList
+              data={patientList}
+              showsVerticalScrollIndicator={false}
+              renderItem={({item, index: _index}) => (
+                <PatientListItem name={item.name} />
+              )}
+              ItemSeparatorComponent={() => <View style={styles.separator} />}
+            />
+          </Card>
+        ) : (
+          <View
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              source={StaticImage.ListEmpty}
+              style={{
+                height: 100,
+                width: 100,
+              }}
+            />
+            <Text
+              style={{
+                color: Colors.Black,
+                fontSize: 18,
+                fontWeight: '600',
+                marginTop: 8,
+              }}>
+              No monitored patients
+            </Text>
+            <Text style={{textAlign: 'center'}}>
+              Add a patient and start monitoring their {'\n'}health.
+            </Text>
+          </View>
+        )}
         <FilledButton
           type="blue"
           label="Add patient"
